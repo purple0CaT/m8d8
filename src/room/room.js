3ef03bd37dd596e8841b17db74872155ebf9895d
Istvan <17463906+purple0CaT@users.noreply.github.com>
2021-11-10 17:54:59 +0200

    get room info

--- src/room/room.js
+++ src/room/room.js
@@ -1,7 +1,9 @@
 import express from "express";
+import RoomSchema from "./schema.js";
 
 const RoomRouter = express.Router();
-RoomRouter.get("/", async (req, res, next) => {
-  res.send("ok");
+RoomRouter.get("/:room", async (req, res, next) => {
+  const room = await RoomSchema.findOne({ room: req.params.room });
+  res.send(room);
 });
 export default RoomRouter;
